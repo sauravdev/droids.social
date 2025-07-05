0042e6073d701da72cfc6a26e1cb2b319d40390b
Hemant1Paliwal <hemant6081@gmail.com>
2025-07-05 15:36:04 +0530

    reduced load time

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    host: '0.0.0.0', // Allow external connections (not just localhost)
-    port: 5173, // Use the same port as your Vite server
-    strictPort: true, // Ensure the server uses the exact specified port
-    cors: {
-      origin: ['https://socialdroids.wisedroids.ai', 'https://your-ngrok-url.ngrok-free.app'],
-      credentials: true, // Allow cookies if needed
-    },
-    allowedHosts: ['socialdroids.wisedroids.ai', 'your-ngrok-url.ngrok-free.app' , "droids.social"],
-  }
-});
+  ...(mode === 'development' && {
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      strictPort: true,
+      cors: {
+        origin: ['https://socialdroids.wisedroids.ai', 'https://your-ngrok-url.ngrok-free.app'],
+        credentials: true,
+      },
+      allowedHosts: ['socialdroids.wisedroids.ai', 'your-ngrok-url.ngrok-free.app', 'droids.social'],
+    }
+  })
+}));
